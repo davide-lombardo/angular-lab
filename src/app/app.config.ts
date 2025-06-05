27f03fbb46d0aabf305494f2b84ccdb6bdb48b57
davide-lombardo <davide.lombardo.92@gmail.com>
2025-06-05 22:16:32 +0200

    uninstall primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import Aura from '@primeng/themes/aura'
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    providePrimeNG({ theme: { preset: Aura } })
   ]
 };
